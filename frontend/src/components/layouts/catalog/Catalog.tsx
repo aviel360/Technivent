169b9123088f3dcb8d4b8f43c724b2e0fdacea9a
aviel360 <aviel3600@gmail.com>
2024-03-23 13:49:59 +0200

    Added events component and api

--- frontend/src/components/layouts/catalog/Catalog.tsx
+++ frontend/src/components/layouts/catalog/Catalog.tsx
@@ -1,48 +1,30 @@
 import React from "react";
 import UserBar from "../../user_bar/UserBar";
-import { Card, Image, Text, Badge, Button, Group, Flex } from "@mantine/core";
+import Events from "../../events/Events";
+import Api from "../../../utils/Api";
 
 interface CatalogProps {}
 
 const Catalog: React.FC<CatalogProps> = () => {
+
+  const apiService = new Api();
+  
+  const fetchData = async () => {
+    let data: EventData[] = [];
+    try {
+      const response = await apiService.getEvents();
+      data = await response.json();
+    } catch(error: any)
+    {
+      window.alert(error);
+    }
+    return data;
+  };
   return (
     <>
       <h1>Catalog</h1>
       <UserBar></UserBar>
-      <Flex
-        mih={50}
-        bg="rgba(0, 0, 0, .3)"
-        gap="md"
-        justify="center"
-        align="center"
-        direction="row"
-        wrap="wrap"
-      >
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-              height={160}
-              alt="Norway"
-            />
-          </Card.Section>
-
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>Norway Fjord Adventures</Text>
-            <Badge color="pink">On Sale</Badge>
-          </Group>
-
-          <Text size="sm" c="dimmed">
-            With Fjord Tours you can explore more of the magical fjord
-            landscapes with tours and activities on and around the fjords of
-            Norway
-          </Text>
-
-          <Button color="blue" fullWidth mt="md" radius="md">
-            Book classic tour now
-          </Button>
-        </Card>
-      </Flex>
+      <Events fetchData={fetchData}/>
     </>
   );
 };
