deb4d5edcc0b9a6b6c07ab76a6f783b3578a98c4
aviel360 <aviel3600@gmail.com>
2024-03-24 18:17:23 +0200

    Backend skeleton, frontend events final touches

--- frontend/src/components/events/Events.tsx
+++ frontend/src/components/events/Events.tsx
@@ -37,21 +37,19 @@ function Events({ fetchData }: EventsProps) {
           <Card key={event.id} shadow="sm" padding="lg" radius="md" withBorder>
             <Card.Section>
               <Image
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                src={event.image}
                 height={160}
-                alt="Norway"
+                alt={event.organizer}
               />
             </Card.Section>
 
             <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>Norway Fjord Adventures</Text>
+              <Text fw={500}>{event.category}</Text>
               <Badge color="pink">On Sale</Badge>
             </Group>
 
             <Text size="sm" c="dimmed">
-              With Fjord Tours you can explore more of the magical fjord
-              landscapes with tours and activities on and around the fjords of
-              Norway
+              {event.description}
             </Text>
 
             <Button color="blue" fullWidth mt="md" radius="md">
--- frontend/src/utils/Types.ts
+++ frontend/src/utils/Types.ts
@@ -1,5 +1,37 @@
-export interface EventData {
-  id: number;
-  title: string;
-  description: string;
+export enum EventCategory {
+  Charity = "Charity Event",
+  Concert = "Concert",
+  Conference = "Conference",
+  Convention = "Convention",
+  Exhibition = "Exhibition",
+  Festival = "Festival",
+  ProductLaunch = "Product Launch",
+  Sports = "Sports Event"
 }
+
+export interface Rating {
+  totalRatings: number,
+  average: number
+}
+
+export interface TicketData {
+  name: string,
+  totalTicket: number,
+  available: number,
+  price: number
+}
+
+export interface EventData {
+  id: string,
+  title: string,
+  category: EventCategory,
+  description: string,
+  organizer: string,
+  start_date: Date,
+  end_date: Date,
+  location: string,
+  ticketArray: TicketData[],
+  image: string,
+  rating: Rating
+}
+
