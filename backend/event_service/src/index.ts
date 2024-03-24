f8da1d133dc8a40272d59f31775c0a9a50e5c7da
aviel360 <aviel3600@gmail.com>
2024-03-24 22:37:01 +0200

    Event backend scheme

--- backend/event_service/src/index.ts
+++ backend/event_service/src/index.ts
@@ -2,8 +2,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 import {
     getEvent,
--- frontend/src/components/user_bar/UserBar.tsx
+++ frontend/src/components/user_bar/UserBar.tsx
@@ -1,6 +1,5 @@
-import { forwardRef } from "react";
 import { ChevronCompactDown, ChevronCompactLeft } from "react-bootstrap-icons";
-import { Group, Text, Menu, UnstyledButton, Button, Flex, Container } from "@mantine/core";
+import { Group, Menu, Button, Flex, Container } from "@mantine/core";
 import { Link } from "react-router-dom";
 import ColorScheme from "../color_scheme/ColorScheme";
 
@@ -8,53 +7,35 @@ interface UserButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   name: string;
 }
 
-const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(({ name, ...others }: UserButtonProps, ref) => (
-  <UnstyledButton
-    ref={ref}
-    style={{
-      padding: "var(--mantine-spacing-md)",
-      color: "var(--mantine-color-text)",
-      borderRadius: "var(--mantine-radius-sm)",
-    }}
-    {...others}
-  >
-    <Group>
-      <div style={{ flex: 1 }}>
-        <Text size="sm" fw={500}>
-          {name}
-        </Text>
-      </div>
-
-      <ChevronCompactDown size="1rem" />
-    </Group>
-  </UnstyledButton>
-));
-
 const UserBar: React.FC<UserButtonProps> = () => {
   return (
     <Container h={50} w="100vw" mt="md">
       <Flex mih={50} align="center" direction="row" justify={"space-between"} wrap="wrap" columnGap={"sm"}>
-        <Menu withArrow>
-          <Menu.Target>
-            <UserButton name="User"/>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Item>
-              <Link to="personal-space">Personal Space</Link>
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-        <Flex
-          direction="row"
-        >
-          <Button variant="light" leftSection={<ChevronCompactLeft />} right={5}>
-              Go back
-          </Button>
-          <ColorScheme/>
-        </Flex>
+        <Button variant="light" leftSection={<ChevronCompactLeft />}>
+          Go back
+        </Button>
+
+        <Group>
+          <Menu withArrow>
+            <Menu.Target>
+              <Button variant="light" rightSection={<ChevronCompactDown size="1rem" />}>
+                Username
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>
+                <Link to="personal-space">Personal Space</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="login">Logout</Link>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+          <ColorScheme />
+        </Group>
       </Flex>
     </Container>
   );
-}
+};
 
 export default UserBar;
