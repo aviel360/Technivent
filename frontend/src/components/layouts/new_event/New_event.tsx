37dabd5cc513dffbb408c5d2336355d202b9d996
edenh <edenh442@gmail.com>
2024-03-26 17:43:32 +0200

    changed date pickers

--- frontend/src/components/layouts/new_event/New_event.tsx
+++ frontend/src/components/layouts/new_event/New_event.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserBar from '../../user_bar/UserBar';
 import NewEventForm from '../../new_event_form/NewEventForm';
+import { EventData } from '../../../utils/Types';
 
 interface NewEventProps {
 }
 
 const NewEvent: React.FC<NewEventProps> = () => {
+    const [eventData, setEventData] = useState<EventData | null>(null);
+    const handleFormSubmit = (data: EventData) => {
+        setEventData(data);
+    };
 
     return (
         <>
             <UserBar name="User"></UserBar>
+            <h1> Create New Event</h1>
             <div className='form-container'>
-                <NewEventForm></NewEventForm>
+                <NewEventForm onSubmit={handleFormSubmit}></NewEventForm>
             </div>
             
         </>
