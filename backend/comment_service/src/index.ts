d64b10bc6443aea37e754289a37a031bcae84035
edenh <edenh442@gmail.com>
2024-03-30 13:35:27 +0300

    Comment api (get comments + post comment)

---
+++ backend/comment_service/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { COMMENT_PATH } from "./const.js";
+import { getComments, addComment } from "./routes.js";
+
+dotenv.config();
+
+let dbUri;
+dbUri = `mongodb+srv://edenh:${process.env.DBPASS}@cluster0.cuoqmgf.mongodb.net/Technivent?retryWrites=true&w=majority&appName=Cluster0`;
+
+await mongoose.connect(dbUri);
+
+const port = process.env.PORT || 3002;
+
+const app = express();
+app.use(express.json());
+
+app.get(COMMENT_PATH, getComments);
+app.post(COMMENT_PATH, addComment);
+
+
+app.listen(port, () => {
+  console.log(`Server running! port ${port}`);
+});
