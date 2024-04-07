be36e400f00cd2a63a413200173202b625c0c39c
aviel360 <aviel3600@gmail.com>
2024-04-07 10:50:27 +0300

    ticket-service skeleton

---
+++ backend/ticket_service/src/routes.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import Payment from "./models/payment.js";
+
+export async function getPayments(req: Request, res: Response) {
+  let dbRes;
+  const username = req.params.username;
+  try {
+    dbRes = await Payment.aggregate([
+      // Match payments based on the specified username
+      {
+        $match: {
+          username: username,
+        },
+      },
+      // Group payments by eventID
+      {
+        $group: {
+          _id: "$eventID", // Group by eventID
+          payments: { $push: "$$ROOT" }, // Store all documents in an array for each eventID
+        },
+      },
+    ]);
+    // Send the result with status 200
+    res.status(200).send({ dbRes });
+  } catch (error: any) {
+    // If there's an error, send an error response with status 500
+    res.status(500).send(error);
+  }
+}
