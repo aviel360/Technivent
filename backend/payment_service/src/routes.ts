3ca8b9d46fdb7496fba71755035fccd86224a62a
edenh <edenh442@gmail.com>
2024-04-07 21:07:27 +0300

    add skeleton to createPayment for payment service

--- backend/payment_service/src/routes.ts
+++ backend/payment_service/src/routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Payment from "./models/payment.js";
+import axios from "axios";
 
 export async function getPayments(req: Request, res: Response) {
   let dbRes;
@@ -25,5 +26,40 @@ export async function getPayments(req: Request, res: Response) {
   } catch (error: any) {
     // If there's an error, send an error response with status 500
     res.status(500).send(error);
+  }
+}
+
+export async function CreatePayment(req: Request, res: Response) {
+  const {username, eventID, creditCardNum, holder, cvv, expDate, ticketId, ticketName, ticketPrice, quantity } = req.body;
+
+  //Check if tickets are still available in the moment of payment
+
+  //check if the tickets are still locked
+
+  //send CC details to payment hammerhead api
+  const totalPrice = quantity * ticketPrice;
+  const paymentResponse = await axios.post('http://localhost:3000/payment', { creditCardNum, holder, cvv, expDate, totalPrice });
+  if(paymentResponse.status == 200){
+    const transactionId = paymentResponse.data; //should we store it in db or just return it to show in suceess page?
+    const paymentData = {
+      eventID,
+      username,
+      date: new Date(),
+      ticketId,
+      totalPrice
+    };
+    const payment = new Payment(paymentData);
+    try {
+      await payment.save();
+      //send success msg to ticket service to unlock the tickets (msgBroker)
+
+      res.status(200).send({ transactionId });
+    } catch (error: any) {
+      res.status(500).send(error); 
+    }
+  }
+  else{
+    //send fail msg to ticket service to unlock the tickets (msgBroker)
+    res.status(500).send("Payment failed");
   }
 }
