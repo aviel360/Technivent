d4146ea234f1b2ba79ba4de536412ed0ab2ab72b
aviel360 <aviel3600@gmail.com>
2024-04-07 16:39:55 +0300

    Revert "prettify"
    This reverts commit 4f363103dfca3f45a262af6a9f8f229b482b8fcf.

--- frontend/src/components/ticket_card/TicketCard.tsx
+++ frontend/src/components/ticket_card/TicketCard.tsx
@@ -1,96 +1,76 @@
-import { Badge, Button, Card, Center, Flex, NumberInput, Text } from "@mantine/core";
-import React, { useState } from "react";
-import { TicketData } from "../../utils/Types";
-import { useNavigate } from "react-router-dom";
+import { Badge, Button, Card, Center, Flex, NumberInput, Text } from '@mantine/core';
+import React, { useState } from 'react';
+import { TicketData } from '../../utils/Types';
+import { useNavigate } from 'react-router-dom';
 
 interface TicketCardProps {
-  ticketArray: TicketData[];
-  isBackOffice: boolean;
-  eventName?: string;
+    ticketArray: TicketData[];
+    isBackOffice: boolean;
+    eventName?: string;
 }
 
-const TicketCard: React.FC<TicketCardProps> = ({ ticketArray, isBackOffice, eventName }) => {
-  const [inputValues, setInputValues] = useState<{ [key: string]: number }>({});
-  const navigate = useNavigate();
+const TicketCard: React.FC<TicketCardProps> = ({ ticketArray, isBackOffice, eventName}) => {
+    const [inputValues, setInputValues] = useState<{ [key: string]: number }>({});
+    const navigate = useNavigate();
+    
+    const handleClick = (inputValue: number, ticketName: string, ticketPrice: number) => {
+        navigate(`/checkout?ticketName=${ticketName}&Price=${ticketPrice}&amount=${inputValue}&event=${eventName}`);
+    };
+    const handleInputChange = (ticketName: string, value: number) => {
+        setInputValues({ ...inputValues, [ticketName]: value });
+    };
+    
+    return (
+        <>
+            <h2>{isBackOffice ? 'Ticket Categories' : 'Buy Tickets:'}</h2>
+            <Flex wrap={"wrap"} direction={"row"} justify={Center} align={Center}>
+                {ticketArray.map((ticket: TicketData) => 
+                (
+                    <Card key={`${ticket._id}-${ticket.name}`} shadow="sm" radius="md" withBorder w={"15rem"} m={"10px"}>
+                    <Card.Section>
+                            <center>
+                                <Badge color="pink" size="xl" p={"md"} mt={"sm"}>{ticket.name}</Badge>
+                            </center>
+                            <Text size="md" fw={400} mt={"sm"}>price: {ticket.price}$</Text>
+                          
+                    </Card.Section>
 
-  const handleClick = (inputValue: number, ticketName: string, ticketPrice: number) => {
-    navigate(`/checkout?ticketName=${ticketName}&Price=${ticketPrice}&amount=${inputValue}&event=${eventName}`);
-  };
-  const handleInputChange = (ticketName: string, value: number) => {
-    setInputValues({ ...inputValues, [ticketName]: value });
-  };
 
-  return (
-    <>
-      <h2>{isBackOffice ? "Ticket Categories" : "Buy Tickets:"}</h2>
-      <Flex wrap={"wrap"} direction={"row"} justify={Center} align={Center}>
-        {ticketArray.map((ticket: TicketData) => (
-          <Card key={`${ticket._id}-${ticket.name}`} shadow="sm" radius="md" withBorder w={"15rem"} m={"10px"}>
-            <Card.Section>
-              <center>
-                <Badge color="pink" size="xl" p={"md"} mt={"sm"}>
-                  {ticket.name}
-                </Badge>
-              </center>
-              <Text size="md" fw={400} mt={"sm"}>
-                price: {ticket.price}$
-              </Text>
-            </Card.Section>
-
-            {isBackOffice ? (
-              //If backOffice than only show available tickets (not able to buy tickets)
-              <Card.Section pb={"20px"}>
-                <Text size="md" fw={400} mt={"md"}>
-                  {" "}
-                  Tickets Available:{" "}
-                </Text>
-                <Text size="md" fw={400}>
-                  {ticket.available.toLocaleString()} / {ticket.totalTickets.toLocaleString()}
-                </Text>
-              </Card.Section>
-            ) : (
-              <Card.Section pb={"20px"}>
-                {ticket.available === 0 ? (
-                  <Text size="lg" fw={700} mt={"sm"}>
-                    Sold out!
-                  </Text>
-                ) : (
-                  <Text size="lg" fw={700} mt={"sm"}>
-                    {ticket.available} tickets left!
-                  </Text>
-                )}
-                <form>
-                  <center>
-                    <NumberInput
-                      label={"Amount of tickets: "}
-                      placeholder="0"
-                      w={"180px"}
-                      p={"md"}
-                      min={0}
-                      max={ticket.available}
-                      disabled={ticket.available === 0}
-                      value={inputValues[ticket.name] || 0}
-                      onChange={(value) => handleInputChange(ticket.name, Number(value))}
-                    ></NumberInput>
-                  </center>
-                  <Button
-                    type="submit"
-                    size="md"
-                    p={"10px"}
-                    radius={"md"}
-                    disabled={ticket.available === 0 || inputValues[ticket.name] === 0}
-                    onClick={() => handleClick(inputValues[ticket.name], ticket.name, ticket.price)}
-                  >
-                    Purchase
-                  </Button>{" "}
-                </form>
-              </Card.Section>
-            )}
-          </Card>
-        ))}
-      </Flex>
+                    {isBackOffice ? (
+                        //If backOffice than only show available tickets (not able to buy tickets)
+                        <Card.Section pb={"20px"}>
+                        <Text size="md" fw={400} mt={"md"}> Tickets Available: </Text>
+                        <Text size="md" fw={400} >{ticket.available.toLocaleString()} / {ticket.totalTickets.toLocaleString()}</Text>
+                        </Card.Section>
+                    ) : (
+                    <Card.Section pb={"20px"}>
+                            {ticket.available === 0 ? (
+                                <Text size="lg" fw={700} mt={"sm"}>Sold out!</Text>
+                            ) : (
+                                <Text size="lg" fw={700} mt={"sm"}>{ticket.available} tickets left!</Text>
+                            )}
+                            <form>
+                                <center>
+                                <NumberInput label={"Amount of tickets: "} placeholder='0' 
+                                    w={"180px"} p={"md"} min={0}  max={ticket.available}
+                                    disabled={ticket.available === 0} 
+                                    value={inputValues[ticket.name] || 0}
+                                    onChange={(value) => handleInputChange(ticket.name, Number(value))}>
+                                </NumberInput>
+                                </center>
+                                <Button type="submit" size="md" p={"10px"} radius={"md"}
+                                        disabled={ticket.available === 0 || inputValues[ticket.name] === 0}
+                                        onClick={() => handleClick(inputValues[ticket.name], ticket.name, ticket.price)}>
+                                    Purchase
+                                </Button> {/*add onClick event*/}
+                            </form>
+                        </Card.Section>
+                    )}
+                    </Card>
+            ))}
+        </Flex>
     </>
-  );
+    );
 };
 
 export default TicketCard;
