4fe7b5b851d74b8f8db64880e608efa46be00dd8
edenh <edenh442@gmail.com>
2024-04-08 16:05:31 +0300

    payment service fucntion

--- backend/payment_service/src/const.ts
+++ backend/payment_service/src/const.ts
@@ -1,3 +1,5 @@
 // Payment
 export const PAYMENT_BY_USER = "/api/payment/:username";
 export const PAYMENT_ROUTE = "/api/payment";
+
+export const HAMMERHEAD_API = "https://www.cs-wsp.net/_functions/pay";
